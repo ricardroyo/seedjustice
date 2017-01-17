9226c2f01ebec3aae4279c650e997b45b11cda5d
ricardroyo <ricardroyo@gmail.com>
2017-01-17 14:05:30 +0100

    Json files to translate the platform

---
+++ json_translate/app_local.js
@@ -0,0 +1,14 @@
+// API connection settings
+app.constant('API_URL', {
+  'url': 'https://lexfunder.atlas.thrinacia.com/api',
+  'loc': '/service/restv1/'
+});
+// Localization settings
+app.constant('LANG', {
+  'PREFERRED_LANG' : 'es',
+  'DEFAULT_LANG' : 'en'
+});
+// CDN settings
+app.constant('CDN', {
+  'APP_URL' : 'https://cdn5.thrinacia.com/**'
+});
